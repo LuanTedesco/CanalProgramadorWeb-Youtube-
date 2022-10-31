bc9ea56dbc95b428b247093fd78eb0b95f8ca6ce
LuanTedesco <luanfelipe2040@gmail.com>
2022-10-31 13:05:50 -0300

    Aula04 - Sintaxe

---
+++ JS/Aula04 - Sintaxe/js/index.js
@@ -0,0 +1,18 @@
+console.log(5 + 5)
+
+console.log('5 + 5')
+
+var a = 1
+var b = 5
+var c = a + b
+
+console.log(c)
+
+var pessoa = 'Luan'
+var PESSOA = 'Tedesco'
+
+console.log(Pessoa)
+
+var primeiroNome
+
+var primeiro_nome
